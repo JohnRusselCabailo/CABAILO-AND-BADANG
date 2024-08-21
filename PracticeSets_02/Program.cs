5756935282ce55d0e6ab41e623fb4e6feacf52f7
Russel <202380167@psu.palawan.edu.ph>
2024-08-21 13:26:21 +0800

    gawa ni cabailo

--- PracticeSets_02/Program.cs
+++ PracticeSets_02/Program.cs
@@ -12,7 +12,7 @@ namespace PracticeSets_02
         {
             for(int i = 0; i <= 20; i++)
             {
-                if(i % 2 == 0)
+                if(i % 2 == 1)
                 {
                     Console.WriteLine(i);
                 }
