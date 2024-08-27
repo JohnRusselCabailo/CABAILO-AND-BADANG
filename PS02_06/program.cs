f7d2be0000e3893f825dbe4031791949f17a6822
ralph angelo badang <Ralphangelobadang06@icloud.com>
2024-08-27 15:56:00 +0800

    added

---
+++ PS02_06/program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS02_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "hplar";
+        
+        if(input == "hplar") {
+            Console.WriteLine($"The String {input} is a palindrome");
+        }
+        
+        else {
+            Console.WriteLine($"The String {input} is not a palindrome");
+        }
+    }
+}
